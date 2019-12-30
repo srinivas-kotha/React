d14bded4a6c0379d11d399ec35b7e3ff2f93ebc4
srinivas-kotha <srinivas.kotha7@gmail.com>
2019-12-30 15:58:04 -0600

    Pagination work

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -5,7 +5,12 @@ import Movies from "./components/movies";
 function App() {
   return (
     <main role="main" className="container">
-      <Movies />
+      <div className="row">
+        <div className="col-2"></div>
+        <div className="col">
+          <Movies />
+        </div>
+      </div>
     </main>
   );
 }
--- vidly/src/components/common/pagination.jsx
+++ vidly/src/components/common/pagination.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 import _ from "lodash";
 
 //Testing git
 const Pagination = props => {
-  const { itemsCount, pageSize } = props;
+  const { itemsCount, pageSize, currentPage, onPageChange } = props;
   const pagesCount = Math.ceil(itemsCount / pageSize);
   if (pagesCount === 1) return null;
   const pages = _.range(1, pagesCount + 1);
@@ -12,13 +13,28 @@ const Pagination = props => {
     <nav aria-label="Page navigation example">
       <ul className="pagination">
         {pages.map(page => (
-          <li className="page-item" key={page}>
-            <button className="page-link b-0">{page}</button>
+          <li
+            className={page === currentPage ? "page-item active" : "page-item"}
+            key={page}
+          >
+            <button
+              className="page-link b-0"
+              onClick={() => onPageChange(page)}
+            >
+              {page}
+            </button>
           </li>
         ))}
       </ul>
     </nav>
   );
+};
+
+Pagination.PropTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired
 };
 
 export default Pagination;
